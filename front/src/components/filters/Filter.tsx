4c318bf7380712bb7b8a98c47917564adf2b7277
ArielPiwko <arielpiwko@gmail.com>
2024-09-09 19:33:16 -0300

    El buscador de tomografias se puede mostrar y ocultar

--- front/src/components/filters/Filter.tsx
+++ front/src/components/filters/Filter.tsx
@@ -27,31 +27,45 @@ const Filter: React.FC<FilterProps> = ({ onFilterChange }) => {
     };
 
     return (
-        <div className="mb-3">
-            <div className="row g-2">
-                <div className="col-12 col-md-4">
-                    <select
-                        value={selectedProperty}
-                        onChange={handlePropertyChange}
-                        className="form-select"
-                    >
-                        <option value="titulo">Titulo</option>
-                        <option value="Codigo De Reporte">Codigo De Reporte</option>
-                        <option value="Estado de informe">Estado de Informe</option>
-                    </select>
-                </div>
+<div className="accordion" id="accordionExample">
+    <div className="accordion-item">
+        <h2 className="accordion-header" id="headingOne">
+        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor:'var(--primary-color-2)'}}>
+            Buscador
+        </button>
+        </h2>
+        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                <div className="mb-3">
+                <div className="row g-2">
+                    <div className="col-12 col-md-4">
+                        <select
+                            value={selectedProperty}
+                            onChange={handlePropertyChange}
+                            className="form-select"
+                        >
+                            <option value="titulo">Titulo</option>
+                            <option value="Codigo De Reporte">Codigo De Reporte</option>
+                            <option value="Estado de informe">Estado de Informe</option>
+                        </select>
+                    </div>
 
-                <div className="col-12 col-md-8">
-                    <input
-                        type="text"
-                        placeholder={`Filtrado por ${selectedProperty}`}
-                        value={filterValue}
-                        onChange={handleFilterValueChange}
-                        className="form-control"
-                    />
+                    <div className="col-12 col-md-8">
+                        <input
+                            type="text"
+                            placeholder={`Filtrado por ${selectedProperty}`}
+                            value={filterValue}
+                            onChange={handleFilterValueChange}
+                            className="form-control"
+                        />
+                    </div>
                 </div>
             </div>
+            </div>
         </div>
+    </div>    
+  </div>
+
     );
 };
 
