25d02304cd8b4a9c9a619b7f41ea140fcfe7c67e
Matias Pardo <pardmatias@frba.utn.edu.ar>
2024-09-23 00:06:44 -0300

    fix diagnostic access

--- front/src/services/BaseClient.ts
+++ front/src/services/BaseClient.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api/` : 'http://134.122.16.32/api/'
+const BASE_URL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api/` : 'http://d-iagnostic.com.ar:8080/api/'
 
 export const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Credentials': 'true',
     'Authorization': 'Bearer ' + localStorage.getItem('token'),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
+    'Content-Type': 'application/json'
   }
 })
 
